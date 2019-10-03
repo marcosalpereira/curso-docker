fbda8c3040e8f3e74972109d87ab65f5d1f19183
Marcos Lacerda <marcos.lacerda@serpro.gov.br>
2019-10-03 10:38:19 -0300

    wip

--- complex/client/src/Fib.js
+++ complex/client/src/Fib.js
@@ -35,7 +35,8 @@ export default class Fib extends Component {
     };
 
     renderSeenIndexes() {
-        return this.state.seenIndexes.join(', ');
+        // return this.state.seenIndexes.join(', ');
+        return this.state.seenIndexes.map(({ number }) => number).join(', ');
     }
 
     renderValues() {
